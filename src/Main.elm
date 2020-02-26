54867cc6ea747b1d0b8dff2d8335e485d8a0c9cc
Jamie Webb <jamie.webb@guardian.co.uk>
2020-02-26 14:56:12 +0000

    working game

--- src/Main.elm
+++ src/Main.elm
@@ -3,45 +3,63 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
-
+import Html.Attributes exposing (style)
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
-type Msg = Click
+type Msg = Click Int
 
+type Player = Naughty | Crossy
 type Square = Blank | Naught | Cross
 
-type alias Grid = List (List Square)
-type alias Model = Grid
+type alias Grid = List Square
+type alias Model = { grid: Grid, player: Player }
 
-init = List.repeat 3 (List.repeat 3 Blank)
+init = { grid = List.repeat 9 Blank, player = Naughty }
 
-viewRow: List Square -> Html Msg
-viewRow squares =
-  div []
-    (List.map viewSquare squares)
+getSquare: Player -> Square
+getSquare player =
+  if (player == Naughty) then Naught else Cross
 
 update: Msg -> Model -> Model
 update msg model =
   case msg of
-    Click -> 
-      updateGrid model
+    Click index -> 
+      { 
+        grid = (List.take index model.grid) ++ [getSquare model.player] ++ (List.drop (index + 1) model.grid),
+        player = if (model.player == Naughty) then Crossy else Naughty
+      }
 
 updateGrid: Grid -> Grid
 updateGrid model =
-  List.repeat 3 (List.repeat 3 Cross)
+  List.repeat 9 Cross
 
-viewSquare: Square -> Html Msg
-viewSquare square =
+viewSquare: Int -> Square -> Html Msg
+viewSquare index square =
+  let 
+    childElements =
+      if modBy 3 (index + 1) == 0 then
+        [br [] []]
+      else
+        []
+  in
+
   case square of
     Blank ->
-      span [ onClick Click ] [ text "⬛️" ]
+        span [ onClick (Click index) ] ([ text "⬛️" ] ++ childElements)
     Cross ->
-      span [] [ text "❌" ]
+      span [] ([ text "❌" ] ++ childElements)
     Naught ->
-      span [] [ text "🔴" ]
+      span [] ([ text "🔴" ] ++ childElements)
 
 view: Model -> Html Msg
 view model =
-  div []
-    (List.map viewRow model)
+  div [
+    style "height" "100vh",
+    style "width" "100vw",
+    style "display" "flex",
+    style "justify-content" "center",
+    style "align-items" "center"]
+    [ div []
+      (List.indexedMap viewSquare model.grid) ]
+  
